ed8d96a0d4b67b9ee17b9cb787d83603b1b3acd2
GillianY1 <48823988+GillianY1@users.noreply.github.com>
2021-08-05 00:29:35 +0800

    Update 2021-08-04T16:29:32.893Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,16 @@
 "use strict";
 
+//import "../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"; 
+//import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
 });
+/*
+let myModal = document.getElementById('addAdmin')
+let myInput = document.getElementById('addInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})*/
 //# sourceMappingURL=all.js.map
